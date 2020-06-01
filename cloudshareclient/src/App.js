f8f5fe124b5cb9aa8869496149c1fed418770c63
Ennyola <medunoyeeni@gmail.com>
2020-06-01 04:25:14 +0100

    Changed Django endpoint url

--- cloudshareclient/src/App.js
+++ cloudshareclient/src/App.js
@@ -20,7 +20,7 @@ import requireAuth from './components/requireAuth'
 
 
 const link = createHttpLink({
-  uri: 'http://localhost:8000/graphiql/',
+  uri: 'https://awploder.herokuapp.com/graphiql/',
 })
 
 const authLink = setContext((_, { headers }) => {
